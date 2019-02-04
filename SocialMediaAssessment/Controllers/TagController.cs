14191cf65a2117f378acf1cc2bffc3a50ad4eb5d
Joshua Morris <jrem94@live.com>
2019-02-04 08:30:27 -0600

    Attempting to fix Automapper error

--- SocialMediaAssessment/Controllers/TagController.cs
+++ SocialMediaAssessment/Controllers/TagController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using SocialMediaAssessment.Services;
 using SocialMediaAssessment.Models;
+using SocialMediaAssessment.DTOs;
 
 namespace SocialMediaAssessment.Controllers
 {
